93060d6617e08ea2d510979b2f40a31563e505c8
AtsushiTsujita <mozu.077@gmail.com>
2026-03-28 11:32:16 +0900

    feat:メールアドレスエラーのテストを作成

--- src/Login.jsx
+++ src/Login.jsx
@@ -12,16 +12,29 @@ const Login = () => {
         formState: { errors },
     } = useForm();
     return (
-        <form onSubmit={handleSubmit(onsubmit)}>
-            <label >パスワード</label>
-            <input {...register('password', { required: '名前を入力してください' })} />
+        <form onSubmit={handleSubmit(onsubmit)} noValidate>
+            <label htmlFor="password">パスワード</label>
+            <input id='password'
+                type="password"  {...register('password', {
+                    required: 'パスワードを入力してください',
+
+                })} />
             {errors.password && <p>{errors.password.message}</p>}
-            <label >メールアドレス</label>
-            <input {...register('email', { required: 'メールを入力してください' })} />
+            <label htmlFor="email">メールアドレス</label>
+            <input
+                id='email'
+                type='email'
+                {...register('email', {
+                    required: '正しいメールを入力してください',
+                    pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: '有効なメールアドレスを設定して。'
+                    }
+                })} />
             {errors.email && <p>{errors.email.message}</p>}
 
             <button type="submit">送信</button>
-        </form>
+        </form >
     );
 };
 
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,3 +1,14 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
+test('ログイン画面にアクセスする', async ({ page }) => {
+  await page.goto('/login');
+
+  await page.getByLabel('パスワード').fill('123');
+
+  await page.getByLabel('メールアドレス').fill('22');
+
+  await page.getByRole('button', { name: '送信' }).click();
+
+  await expect(page.getByText('有効なメールアドレスを設定して。')).toBeVisible();
+});
